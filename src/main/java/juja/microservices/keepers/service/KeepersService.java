3b46201d42b7aa0a58b4dabbc662614456857d05
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-06-07 12:58:14 +0300

    #6 Added spring annotation.

--- src/main/java/juja/microservices/keepers/service/KeepersService.java
+++ src/main/java/juja/microservices/keepers/service/KeepersService.java
@@ -1,8 +1,16 @@
 package juja.microservices.keepers.service;
 
+import juja.microservices.keepers.dao.KeepersRepository;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
 /**
  * @author Vadim Dyachenko
  */
+@Service
 public class KeepersService {
+    @Inject
+    private KeepersRepository keepersRepository;
     //TODO Should be implemented internal service logic
 }
