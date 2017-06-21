57224f6a52507ea6ad76965189b85bc74261585d
User <netangel@inbox.ru>
2017-06-21 23:23:35 +0300

    #11 Added integration tests for KeepersController class

--- src/test/java/juja/microservices/integration/KeepersControllerIntegrationTest.java
+++ src/test/java/juja/microservices/integration/KeepersControllerIntegrationTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class KeepersControllerIntegrationTest extends BaseIntegrationTest {
 
     private static final String ADD_KEEPER_URL = "/keepers";
+    private static final String GET_DIRECTIONS_URL = "/keepers/1111A9999";
     private MockMvc mockMvc;
 
     @Rule
@@ -34,13 +35,10 @@ public class KeepersControllerIntegrationTest extends BaseIntegrationTest {
     }
 
     @Test
-    @UsingDataSet(locations = "/datasets/initEmptyDb.json")
-    public void addKeeperTest() throws Exception {
-
-        String jsonContentRequest ="{\"from\":\"admin\",\"uuid\":\"12345\",\"direction\":\"LMS\"}";
-        mockMvc.perform(post(ADD_KEEPER_URL)
-                .contentType(APPLICATION_JSON_UTF8)
-                .content(jsonContentRequest))
+    @UsingDataSet(locations = "/datasets/getKeeperDirections.json")
+    public void getDirectionsAndReturnJson() throws Exception {
+        mockMvc.perform(get(GET_DIRECTIONS_URL)
+                .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(content().contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
     }
