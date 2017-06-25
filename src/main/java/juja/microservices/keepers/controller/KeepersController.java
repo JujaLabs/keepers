6f37b4d69dd589c598224744255968218a9b0ecc
User <netangel@inbox.ru>
2017-06-25 19:59:30 +0300

    #11 Fixed code

--- src/main/java/juja/microservices/keepers/controller/KeepersController.java
+++ src/main/java/juja/microservices/keepers/controller/KeepersController.java
@@ -3,6 +3,7 @@ package juja.microservices.keepers.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import juja.microservices.keepers.entity.KeeperRequest;
 import juja.microservices.keepers.service.KeepersService;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
@@ -10,7 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,6 +26,19 @@ public class KeepersController {
 
     @Inject
     private KeepersService keepersService;
+
+    @PostMapping(value = "/keepers", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> addKeeper(@Valid @RequestBody KeeperRequest request) {
+        //TODO Should be implemented feature KPR-F1
+        List<String> ids = new ArrayList<>();
+        return ResponseEntity.ok(ids);
+    }
+
+    @PutMapping(value = "/keepers", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> updateKeeper(@Valid @RequestBody KeeperRequest request) {
+        //TODO Should be implemented feature KPR-F2
+        return null;
+    }
 
     @GetMapping(value = "/keepers/{uuid}", produces = "application/json")
     @ApiOperation(
@@ -38,5 +54,11 @@ public class KeepersController {
         List result = keepersService.getDirections(uuid);
         logger.info("Request for active directions for keeper with uuid ", uuid);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/keepers", produces = "application/json")
+    public ResponseEntity<?> getKeepers() {
+        //TODO Should be implemented feature KPR-F4
+        return null;
     }
 }
