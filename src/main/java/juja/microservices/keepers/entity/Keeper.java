f2477540f19b1ff99226829115754ddc33c5bbfc
User <netangel@inbox.ru>
2017-06-26 19:34:08 +0300

    #11 Added Keeper class

---
+++ src/main/java/juja/microservices/keepers/entity/Keeper.java
@@ -0,0 +1,34 @@
+package juja.microservices.keepers.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author Vadim Dyachenko
+ * @author Dmitriy Roy
+ */
+@Data
+@AllArgsConstructor
+public class Keeper {
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("uuid")
+    private String uuid;
+    @JsonProperty("from")
+    private String from;
+    @JsonProperty("direction")
+    private String direction;
+    @JsonProperty("startDate")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    private Date startDate;
+    @JsonProperty("dismissDate")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    private Date dismissDate;
+    @JsonProperty("isActive")
+    private boolean isActive;
+
+}
