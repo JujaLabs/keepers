6381d4bd0d471d5c233cb5c4023591cdc63cda1c
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-06-29 22:22:18 +0300

    #12: add @Data onto Keeper.class

--- src/main/java/juja/microservices/keepers/entity/Keeper.java
+++ src/main/java/juja/microservices/keepers/entity/Keeper.java
@@ -3,6 +3,8 @@ package juja.microservices.keepers.entity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
@@ -11,9 +13,11 @@ import java.time.ZoneId;
 import java.util.Date;
 
 /**
+ * @author Vadim Dyachenko
+ * @author Dmitriy Roy
  * @author Dmitriy Lyashenko
  */
-
+@Data
 @Getter
 @ToString
 public class Keeper {
@@ -22,7 +26,9 @@ public class Keeper {
     private String id;
 
     private String from;
+    @JsonProperty("uuid")
     private String uuid;
+    @JsonProperty("direction")
     private String direction;
 
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
