b7f425ef2d13bd99bf29a189bc8f48cf7fdee049
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-07-02 18:14:36 +0300

    #12: add Swagger annotations to the method KeepersController.getKeepers()

--- src/main/java/juja/microservices/keepers/controller/KeepersController.java
+++ src/main/java/juja/microservices/keepers/controller/KeepersController.java
@@ -62,6 +62,16 @@ public class KeepersController {
     }
 
     @GetMapping(value = "/keepers", produces = "application/json")
+    @ApiOperation(
+            value = "Get all active keepers",
+            notes = "This method have to get all active keepers"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "Returns maps with active keepers id and their cctive directions"),
+            @ApiResponse(code = HttpURLConnection.HTTP_BAD_REQUEST, message = "Bad request"),
+            @ApiResponse(code = HttpURLConnection.HTTP_BAD_METHOD, message = "Bad method"),
+            @ApiResponse(code = HttpURLConnection.HTTP_UNSUPPORTED_TYPE, message = "Unsupported request media type")
+    })
     public ResponseEntity<?> getKeepers() {
         return ResponseEntity.ok(keepersService.getAllActiveKeepers());
     }
