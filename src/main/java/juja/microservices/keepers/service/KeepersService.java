d6dff653d7e0f1088c3b89a5be8c1c8763e7b36d
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-07-05 00:08:01 +0300

    #26: delete some import

--- src/main/java/juja/microservices/keepers/service/KeepersService.java
+++ src/main/java/juja/microservices/keepers/service/KeepersService.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import javax.inject.Inject;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
