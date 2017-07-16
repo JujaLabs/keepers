b003bd420b6b5f98ca46ee2ded0f4f3420162159
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-07-16 14:33:29 +0300

    #26: clean import

--- src/test/java/juja/microservices/keepers/service/KeepersServiceTest.java
+++ src/test/java/juja/microservices/keepers/service/KeepersServiceTest.java
@@ -24,10 +24,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 
 import static org.mockito.Matchers.anyString;
 
