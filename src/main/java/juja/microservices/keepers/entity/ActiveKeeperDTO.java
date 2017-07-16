35432f71d2dc86966afb6038127993ca17a7ce59
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-07-16 14:34:00 +0300

    #26: clean import

--- src/main/java/juja/microservices/keepers/entity/ActiveKeeperDTO.java
+++ src/main/java/juja/microservices/keepers/entity/ActiveKeeperDTO.java
@@ -3,7 +3,6 @@ package juja.microservices.keepers.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
