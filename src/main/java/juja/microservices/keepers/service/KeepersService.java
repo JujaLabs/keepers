1dd9b2939d0ad2691538eae36314a426f0e25c9e
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-07-17 21:20:33 +0300

    #26: unfolded imports

--- src/main/java/juja/microservices/keepers/service/KeepersService.java
+++ src/main/java/juja/microservices/keepers/service/KeepersService.java
@@ -10,7 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import javax.inject.Inject;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 
 /**
  * @author Vadim Dyachenko
